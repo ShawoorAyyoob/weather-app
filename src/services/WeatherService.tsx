7d8f25cec21c4fac247477f77f70cd1ba0fd5c71
Shawoor Kasim <mohdshawoor@gmail.com>
2025-10-21 11:12:51 +0400

    clearing errors

--- src/services/WeatherService.tsx
+++ src/services/WeatherService.tsx
@@ -1,12 +1,5 @@
-import axios from 'axios';
-import type Weather from '../models/Weather';
+import axios from "axios";
 
 const API_KEY = "a93645c644044343bbe73310251310";
-const BASE_URL = "https://api.weatherapi.com";
+const BASE_URL = "https://api.weatherapi.com/v1/current.json";
 
-export const fetchWeather = async (city:string) =>{
-    const response = await axios.get<Weather>(`${BASE_URL}/current.json`, {
-        params: {key: API_KEY, q: city}
-        });
-        return response.data;
-    }
